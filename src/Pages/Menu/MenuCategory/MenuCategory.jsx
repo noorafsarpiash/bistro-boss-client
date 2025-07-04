6f2faaeb02c86768d623a39b907e17bdb1c31c50
Noor Piash <noorpiash1234@gmail.com>
2025-07-04 23:44:29 +0600

    65-4

--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
 
-const MenuCategory = () => {
+import Cover from '../../Shared/Cover/Cover'
+import MenuItem from '../../Shared/MenuItem/MenuItem'
+
+const MenuCategory = ({ items, title, img }) => {
     return (
-        <div>MenuCategory</div>
+
+
+        <div className='pt-8'>
+            {title && <Cover img={img} title={title} />}
+            <div className='grid md:grid-cols-2 gap-10 my-16'>
+                {items.map(item => <MenuItem key={item._id}
+                    item={item}></MenuItem>)}
+            </div>
+        </div>
     )
 }
 
